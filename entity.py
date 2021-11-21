ffaafa69359b4b7992bc36c750ed288ec4f5e2be
forget2save <3170105731@zju.edu.cn>
2021-11-22 00:54:30 +0800

    resolve the citation of person

--- entity.py
+++ entity.py
@@ -1,9 +1,16 @@
 class Paper:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, title, authors, fields, abstract, abs_link, pdf_link) -> None:
+        self.title = title
+        self.authors = authors
+        self.fields = fields
+        self.abstract = abstract
+        self.abs_link = abs_link
+        self.pdf_link = pdf_link
+
 
 class Author:
-    def __init__(self) -> None:
-        pass
-
-
+    def __init__(self, profile_link, affiliation, interest, citation) -> None:
+        self.profile_link = profile_link
+        self.affiliation = affiliation
+        self.interest = interest
+        self.citation = citation
--- paper_finder.py
+++ paper_finder.py
@@ -1,32 +1,60 @@
 import bs4
 import requests
 import re
+from entity import Author, Paper
 
 
-def get_arxiv_links(tag: bs4.element.Tag):
+def get_arxiv_info(tag: bs4.element.Tag) -> Paper:
     title = tag.find(class_="title").text.strip()
-    # don't worry! papers that have single author are also named by "authors" in arxiv.
     authors = [
-        t.strip() for t in tmp.find(
+        t.strip() for t in tag.find(
             class_="authors").text.replace("Authors:", "").split(",")
     ]
-    fields = [t.attrs["data-tooltip"] for t in tag.find_all(class_="tag")]
+    fields = [t.text for t in tag.find_all(class_="tag")]
+    abstract = tag.find(class_="abstract-full").text[:-10].strip()
     abs_link = tag.find(href=re.compile("abs")).attrs["href"]
     pdf_link = tag.find(href=re.compile("pdf")).attrs["href"]
-    return title, abs_link, pdf_link, fields, authors
+    return Paper(title, authors, fields, abstract, abs_link, pdf_link)
 
 
-proxy = {"http": "127.0.0.1:7890"}
-arxiv = "https://arxiv.org/search/"
-keyword = "cam"
-params = {
-    "query": keyword,
-    "source": "header",
-    "searchtype": "all",
-}
-r = requests.get(arxiv, proxies=proxy, params=params)
-# https://arxiv.org/search/?query=cam&source=header&searchtype=all
-soup = bs4.BeautifulSoup(r.text, "html.parser")
+def get_scholar_info(tag: bs4.element.Tag):
+    profile_link = tag.find(class_="gs_ai_name").a.attrs["href"]
+    affiliation = tag.find(class_="gs_ai_aff").text
+    interest = [x.text for x in tag.find_all(class_="gs_ai_one_int")]
+    citation = int(tag.find(class_="gs_ai_cby").text[8:].strip())
+    return Author(profile_link, affiliation, interest, citation)
+
+
+def query_arxiv(keyword):
+    site = "https://arxiv.org/search/"
+    proxy = {"http": "127.0.0.1:7890"}
+    params = {
+        "query": keyword,
+        "source": "header",
+        "searchtype": "all",
+    }
+    r = requests.get(site, proxies=proxy, params=params)
+    soup = bs4.BeautifulSoup(r.text, "html.parser")
+    return soup
+
+
+def query_scholar(author):
+    site = "https://scholar.google.com/citations"
+    proxy = {"http": "127.0.0.1:7890"}
+    params = {
+        "mauthors": author.replace(" ", "+"),
+        "view_op": "search_authors",
+        "hl": "en",
+    }
+    r = requests.get(site, proxies=proxy, params=params)
+    soup = bs4.BeautifulSoup(r.text, "html.parser")
+    return soup
+
+
+soup = query_arxiv("cam")
 page_results = soup.find_all("li", class_="arxiv-result")
-tmp = page_results[0]
-get_arxiv_links(tmp)
+papers = [get_arxiv_info(x) for x in page_results]
+
+soup = query_scholar(papers[0].authors[0])
+page_results = soup.find_all(class_="gs_ai_t")
+
